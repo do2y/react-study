d0e7f179731275a066ce5ac6bdb8f5ed71438885
do2y <essen030216@naver.com>
2026-06-09 14:10:24 +0900

    feat: 영화 검색 기능 구현

--- react-netflix-clone/src/components/Nav.js
+++ react-netflix-clone/src/components/Nav.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./Nav.css";
+import { useNavigate } from "react-router-dom";
 
 function Nav() {
   const [show, setShow] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate(); //라우팅을 코드로 제어할 때 사용. 로직 실행 후 이동하게 하고싶을 때.
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -19,6 +22,11 @@ function Nav() {
     };
   }, []);
 
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`); //url 이동
+  };
+
   return (
     <nav className={`nav ${show && "nav__black"}`}>
       <img
@@ -27,6 +35,15 @@ function Nav() {
         className="nav__logo"
         onClick={() => window.location.reload()}
       />
+
+      <input
+        value={searchValue}
+        onChange={handleChange}
+        className="nav__input"
+        type="text"
+        placeholder="영화를 검색해주세요."
+      />
+
       <img
         alt="User logged"
         src="https://i.pinimg.com/564x/1b/a2/e6/1ba2e6d1d4874546c70c91f1024e17fb.jpg"
