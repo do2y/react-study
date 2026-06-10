576b86799dc559e9d48f1798590898588c13648c
do2y <essen030216@naver.com>
2026-06-10 16:37:16 +0900

    refactor: React import 제거

--- react-netflix-clone/src/components/Row.js
+++ react-netflix-clone/src/components/Row.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "../api/axios";
 import "./Row.css";
 import MovieModal from "../MovieModal";
